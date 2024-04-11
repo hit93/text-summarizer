867f5b0e2b085a12a085f7c10b215525366a2d6a
hit93 <hitesh_93@live.com>
2024-04-11 20:00:05 +0530

    added model training

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from textSummarizer.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.data_validation import DataValidationPipeline
+from textSummarizer.pipeline.data_transformation import DataTransformationPipeline
+from textSummarizer.pipeline.model_trainer import ModelTrainingPipeline
 from textSummarizer.logging import logger
 
 Stage_name = "Data Ingestion stage"
@@ -19,6 +21,30 @@ try:
     logger.info(f">>>>>> stage {Stage_name} started <<<<<<")
     data_validation = DataValidationPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {Stage_name} completed <<<<<<\n\nx==========x")
+except Exception as e:  
+    logger.exception(e)
+    raise e
+
+Stage_name = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {Stage_name} started <<<<<<")
+    data_transformation = DataTransformationPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage {Stage_name} completed <<<<<<\n\nx==========x")
+except Exception as e:  
+    logger.exception(e)
+    raise e
+
+
+
+Stage_name = "Model training stage"
+
+try:
+    logger.info(f">>>>>> stage {Stage_name} started <<<<<<")
+    model_training = ModelTrainingPipeline()
+    model_training.main()
     logger.info(f">>>>>> stage {Stage_name} completed <<<<<<\n\nx==========x")
 except Exception as e:  
     logger.exception(e)
---
+++ src/textSummarizer/pipeline/model_trainer.py
@@ -0,0 +1,10 @@
+from textSummarizer.components.model_trainer import ModelTrainer
+from textSummarizer.config.configuration import ConfigurationManager
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
